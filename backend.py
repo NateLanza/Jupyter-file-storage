3adb03104f4fdf1d71b6105bea0bf103dd6ad5ca
NateLanza <natelanzadevelopment@gmail.com>
2025-01-11 09:37:37 -0700

    Working synchronized dictionary

--- backend.py
+++ backend.py
@@ -3,7 +3,7 @@ import traitlets
 
 class ExampleWidget(anywidget.AnyWidget):
   _esm = "frontend.js"
-  value = traitlets.List(["ex"]).tag(sync=True)
+  value = traitlets.Dict({"ab": "true"}).tag(sync=True)
   def __init__(self, **kwargs):
     super().__init__(**kwargs)
     self.observe(lambda change: print(change), names="value")
--- frontend.ts
+++ frontend.ts
@@ -3,17 +3,25 @@ import { AnyModel } from "./AnyWidgetTypes";
 function render({ model, el }: { model: AnyModel, el: Element}) {
   let button = document.createElement("button");
   button.innerHTML = "Send";
+  let clearButton = document.createElement("button");
+  clearButton.innerHTML = "Clear";
   let textField = document.createElement("input");
+
   button.addEventListener("click", () => {
-    let newVal = [...(model.get("value") as string[]), textField.value];
+    let newVal = {...(model.get("value") as Object), ...{test: textField.value}};
     model.set("value", newVal);
     model.save_changes();
   });
+  clearButton.addEventListener("click", () => {
+    model.set("value", {});
+    model.save_changes();
+  })
   model.on("change: value", () => {
     console.log("Value changed to: ", model.get("value"));
   });
-  el.classList.add("counter-widget");
+
   el.appendChild(button);
   el.appendChild(textField);
+  el.appendChild(clearButton);
 }
 export default { render };
