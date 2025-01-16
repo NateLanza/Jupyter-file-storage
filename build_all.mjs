9e91ba6ab39a75ea4fdd5d6eecd7b5b93694ac26
NateLanza <natelanzadevelopment@gmail.com>
2025-01-16 16:27:04 -0700

    Backup of untested build files

---
+++ build_all.mjs
@@ -0,0 +1,25 @@
+import fs from 'fs';
+import * as c from './vite.config.mjs';
+import { build } from 'vite';
+
+const { configCreator } = c;
+
+const entryPaths = {
+  persist_output: {
+    srcFileName: "frontend.ts",
+    dir: "./lib",
+    fileName: "frontend.js"
+  }
+};
+
+let first = true;
+
+const entries = Object.entries(entryPaths);
+
+for (let i = 0; i < entries.length; ++i) {
+  const [name, entry] = entries[i];
+  const config = configCreator(entry, first);
+  first = false;
+  console.log(`Building: ${name} (${i + 1}/${entries.length})`);
+  await build(config);
+}
