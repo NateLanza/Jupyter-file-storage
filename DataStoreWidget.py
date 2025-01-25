1071d8216e551788464d38e17378f4d50005fbda
NateLanza <natelanzadevelopment@gmail.com>
2025-01-25 14:02:40 -0700

    Globalize persist file name and start working on file saving

--- DataStoreWidget.py
+++ DataStoreWidget.py
@@ -5,30 +5,43 @@ from traitlets import observe
 
 FILE_EXTENSION = ".jupersist"
 
+def init_data_store(file_name: str):
+  if type(file_name) is not str:
+    raise ValueError("file_name must be a string")
+  
+  if '.' in file_name or '/' in file_name or '\\' in file_name:
+    raise ValueError("file_name cannot contain '.' or '/' or '\\'")
+  
+  file_name = file_name + FILE_EXTENSION
+
+  with open(file_name, 'wb') as file:
+    pkl.dump({}, file)
+  
+  global dsw_file_name
+  dsw_file_name = file_name
+
 class DataStoreWidget(anywidget.AnyWidget):
   dsw_data_store = traitlets.Dict({}).tag(sync=True)
   dsw_id = traitlets.Unicode("").tag(sync=True)
-  def __init__(self, file_name: str, id: str, **kwargs):
+  def __init__(self, id: str, **kwargs):
     self.dsw_id = id
     super().__init__(**kwargs)
 
     if type(self) is DataStoreWidget:
       raise NotImplementedError("Cannot directly instantiate abstract class DataStoreWidget")
-    
-    if type(file_name) is not str:
-      raise ValueError("file_name must be a string")
-    
-    if '.' in file_name or '/' in file_name or '\\' in file_name:
-      raise ValueError("file_name cannot contain '.' or '/' or '\\'")
-    
-    self.file_name = file_name + FILE_EXTENSION
+        
+    if 'dsw_file_name' not in globals():
+      raise ValueError("DataStoreWidget must be initialized with init_data_store before use")
 
-    # with open(self.file_name, 'rb') as file:
-    #   if file.readable():
-    #     self.dsw_data_store = pkl.load(file)
+    global dsw_file_name
+    with open(dsw_file_name, 'rb') as file:
+      if file.readable():
+        self.global_data_store = pkl.load(file)
+        self.dsw_data_store = self.global_data_store.get(self.dsw_id, {})
 
   @observe('dsw_data_store')
   def data_changed(self, change: Dict):
     print(change)
-    # with open(self.file_name, 'wb') as file:
-    #   pkl.dump(self.dsw_data_store, file)
+    self.global_data_store[self.dsw_id] = self.dsw_data_store
+    with open(dsw_file_name, 'wb') as file:
+      pkl.dump(self.global_data_store, file)
--- backend.py
+++ backend.py
@@ -2,5 +2,5 @@ from DataStoreWidget import DataStoreWidget
 
 class ExampleWidget(DataStoreWidget):
   _esm = "lib/frontend.js"
-  def __init__(self, file_name: str, id: str, **kwargs):
-    super().__init__(file_name, id, **kwargs)
+  def __init__(self, id: str, **kwargs):
+    super().__init__(id, **kwargs)
